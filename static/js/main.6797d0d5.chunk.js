0c30f396963a43be72784fa1953fdc78f61cf661
Tanktwo <rhkdtjr9@naver.com>
2021-06-23 23:50:22 +0900

    Updates

--- static/js/main.6797d0d5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpttb-frontend"]=this["webpackJsonpttb-frontend"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),o=(n(8),n(0));var d=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("header",{className:"App-header",children:"Tanktwo's Blog"})})};a.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.6797d0d5.chunk.js.map
