62c663a04963e2e50833a4f8f08a5be9e83d0a45
Tanktwo <rhkdtjr9@naver.com>
2021-06-30 01:21:31 +0900

    sonar qube

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function apiTest() {
+    fetch('http://tanktwo.synology.me:5001/users').then(res=> res.json()).then(json => console.log(json)).catch(erro=> console.error(erro))
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Tanktwo's Blog
+        <button onClick={apiTest} >
+        http://tanktwo.synology.me/users
+        </button>
       </header>
+    
     </div>
   );
 }
