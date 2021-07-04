b88dad7fe8332ec7225c393dcaae13cc024dde4f
Tanktwo2 <rhkdtjr9@naver.com>
2021-07-05 00:47:06 +0900

    [ttb-002] write api

--- src/pages/note/components/WriteNote.tsx
+++ src/pages/note/components/WriteNote.tsx
@@ -29,9 +29,28 @@ export default function WriteNote({
         })
     }
 
+    async function onSubmitNoteData(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const res = await fetch(
+            'https://tanktwo.synology.me:5001/ttb/note/write',
+            {
+                method: 'POST',
+                body: JSON.stringify(noteData),
+            }
+        )
+        if (res.status >= 400) {
+            alert(`error : ${res.status}`)
+        } else {
+            alert(`succcess`)
+        }
+    }
+
     return (
         <>
-            <form className={`modal ${isActive && 'is-active'}`}>
+            <form
+                className={`modal ${isActive && 'is-active'}`}
+                onSubmit={onSubmitNoteData}
+            >
                 <div className="modal-background"></div>
                 <div className="modal-card">
                     <header className="modal-card-head">
@@ -49,6 +68,7 @@ export default function WriteNote({
                         <button
                             className="delete"
                             aria-label="close"
+                            type="button"
                             onClick={onActiveControl}
                         ></button>
                     </header>
@@ -64,10 +84,15 @@ export default function WriteNote({
                         <button
                             className="button is-success"
                             disabled={password !== 'notepw'}
+                            type="submit"
                         >
                             Save
                         </button>
-                        <button className="button" onClick={onActiveControl}>
+                        <button
+                            className="button"
+                            type="button"
+                            onClick={onActiveControl}
+                        >
                             Cancel
                         </button>
                     </footer>
