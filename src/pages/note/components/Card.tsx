e3baa5be820a77749de4749c297a3172ce8ca749
Tanktwo2 <rhkdtjr9@naver.com>
2021-07-21 00:13:54 +0900

    [ttb-004] : feature : card style

--- src/pages/note/components/Card.tsx
+++ src/pages/note/components/Card.tsx
@@ -7,6 +7,18 @@ export default function Card({ noteList }: { noteList: NoteDto }) {
             <div className="card">
                 <header className="card-header">
                     <p className="card-header-title">{noteList.title}</p>
+                    <p style={{ margin: 10 }}>
+                        {noteList &&
+                            noteList.tags.map((row) => (
+                                <small>
+                                    <a href={`#${row.tag}`}>#{row.tag}</a>&nbsp;
+                                </small>
+                            ))}
+                    </p>
+                    <br />
+                    <p style={{ margin: 10 }}>
+                        {noteList.modifiedAt.slice(0, 10)}
+                    </p>
                 </header>
                 <div className="card-content">
                     <div className="content">{noteList.content}</div>
